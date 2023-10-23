72c7c16d0ce926ee18ce1c21e899a1ce1f6946f4
Tianyang Zhang <tianyangzhang515@gmail.com>
2023-10-23 12:13:18 -0700

    Add input for generating new grid

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 const grid = document.querySelector(".grid");
+const gridSizeInput = document.querySelector("#grid-size");
+const generateButton = document.querySelector("#generate");
 
-for (let i = 0; i < 16; i++)
-{
-    let row = document.createElement("div");
-    for (let j = 0; j < 16; j++)
-    {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", function() {
-            square.style.backgroundColor = "#555";
-        });
-        row.appendChild(square);
+function generateGrid(size) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
-    row.classList.add("row");
-    grid.appendChild(row);
+    for (let i = 0; i < size; i++)
+    {
+        let row = document.createElement("div");
+        for (let j = 0; j < size; j++)
+        {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", function() {
+                square.style.backgroundColor = "#555";
+            });
+            row.appendChild(square);
+        }
+        row.classList.add("row");
+        grid.appendChild(row);
+    }
 }
+
+generateButton.addEventListener("click", function() {
+    let gridSize = Number.parseInt(gridSizeInput.value);
+    if (Number.isNaN(gridSize)) {
+        return;
+    }
+    if (gridSize < 1) {
+        gridSize = 1;
+    }
+    if (gridSize > 64) {
+        gridSize = 64;
+    }
+    generateGrid(gridSize);
+});
+
+generateGrid(16);
