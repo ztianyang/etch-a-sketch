4506011097273b8e90518127efdddab37dd5ca19
Tianyang Zhang <tianyangzhang515@gmail.com>
2023-10-24 20:39:46 -0700

    Add checkerboard background and rainbow color

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const grid = document.querySelector(".grid");
 const gridSizeInput = document.querySelector("#grid-size");
 const generateButton = document.querySelector("#generate");
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function generateGrid(size) {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
@@ -13,8 +17,14 @@ function generateGrid(size) {
         {
             let square = document.createElement("div");
             square.classList.add("square");
+            if (i % 2 !== j % 2) {
+                square.style.backgroundColor = "#eee";
+            }
             square.addEventListener("mouseover", function() {
-                square.style.backgroundColor = "#555";
+                let red = randomNumber(0, 255);
+                let green = randomNumber(0, 255);
+                let blue = randomNumber(0, 255);
+                square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
             });
             row.appendChild(square);
         }
